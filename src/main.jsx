4e35cd0feb080e75daf32c6f9a5669820f3bb5c7
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-08 16:10:22 +0300

    feat: add primary code for redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import { tasks } from './reducers'
+
+const store = createStore(tasks);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

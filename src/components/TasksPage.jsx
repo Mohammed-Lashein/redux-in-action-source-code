bfd872d3f94676e4b4c4aabcf655943efc2ea580
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-09 15:40:03 +0300

    feat: add preliminary code for rendering the add task form

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -3,6 +3,18 @@ import TaskList from "./TaskList.jsx"
 const TASK_STATUSES = ["Unstarted", "In Progress", "Completed"]
 class TasksPage extends Component {
 	// Why didn't we need to call the constructor here ? TO UNDERSTAND
+	// => We needn't to interact with the props pased to this component, so react internally called super(props)
+	// for us .
+	// But things will change now as we need to read some values from the props
+
+	constructor(props) {
+		super(props)
+	}
+	state = {
+		showNewCardForm: false,
+		cardTitle: "",
+		cardDescription: "",
+	}
 	renderTaskLists() {
 		const { tasks } = this.props
 		return TASK_STATUSES.map((status) => {
@@ -16,12 +28,38 @@ class TasksPage extends Component {
 			)
 		})
 	}
+	handleNewTaskAddition = (e) => {
+		e.preventDefault()
+		console.log("handling new task submission !")
+	}
+	renderNewTaskForm() {
+		return (
+			<form
+				action=''
+				onSubmit={this.handleNewTaskAddition}
+			>
+				<button>+ New Task</button>
+				<input
+					type='text'
+					placeholder='title'
+				/>
+				<input
+					type='text'
+					placeholder='description'
+				/>
+				<button>save</button>
+			</form>
+		)
+	}
 	state = {}
 	render() {
 		return (
-			<div className='tasks'>
-				<div className='task-lists'>{this.renderTaskLists()}</div>
-			</div>
+			<>
+				<div className='tasks'>
+					{this.renderNewTaskForm()}
+					<div className='task-lists'>{this.renderTaskLists()}</div>
+				</div>
+			</>
 		)
 	}
 }

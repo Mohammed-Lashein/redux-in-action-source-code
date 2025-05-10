c6c0e1453c6584141f4edc6eae087cce7fd06c0f
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-05-10 15:46:30 +0300

    feat: add redux-devtools using applyMiddleware

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import { tasks } from "./reducers"
 import { Provider } from "react-redux"
+import { composeWithDevTools } from "@redux-devtools/extension"
 
-const store = createStore(tasks)
+// prettier-ignore to follow the docs format for the code
+
+const store = createStore(
+	tasks,
+	composeWithDevTools(applyMiddleware())
+	/* 
+		In the docs they are writing it like so :  applyMiddleware(...middleware),
+		but my intuition was correct that this syntax will throw reference error 'middleware' is not defined
+	*/
+)
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>

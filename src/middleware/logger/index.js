66906c862430701d4d45b7eb9e44bc8d0a1d200d
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-12 17:36:51 +0300

    docs: add a comment to logger middleware

--- src/middleware/logger/index.js
+++ src/middleware/logger/index.js
@@ -5,6 +5,12 @@ export const logger = store => next => action => {
   const resultOfTheDispatchedAction = next(action)
   console.log('next state', store.getState());
   console.groupEnd()
-
+  /* 
+  Why do we need to return next(action) from the middleware ?
+  In order for the middleware chain to work.
+  After logging the return value of next(action), it printed out the action object of each action type
+  */
+  // console.log(resultOfTheDispatchedAction);
+  
   return resultOfTheDispatchedAction
 }

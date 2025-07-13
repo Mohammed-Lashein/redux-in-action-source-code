19d7ef43b0cdc033f62fdd2db94ff9e6dc69a9b9
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-13 18:38:30 +0300

    feat: complete the logic for apiMiddleware for fetchTasks action creator

--- src/middleware/apiMiddleware/api.js
+++ src/middleware/apiMiddleware/api.js
@@ -2,5 +2,30 @@ import { graphqlClient } from "../../graphqlClient"
 
 export const CALL_API = "Call Api"
 
-export const apiMiddleware = (store) => (next) => (action) => {
+export const apiMiddleware = (store) => (next) => async (action) => {
+	const callApi = action[CALL_API]
+	if (typeof callApi === "undefined") {
+		return next(action)
+	}
+
+	const { query, variables } = callApi
+	const [requestStartedType, succeededType, failureType] = callApi.types
+	debugger
+	next({ type: requestStartedType })
+
+	const { data, error } = await graphqlClient({ query, variables })
+
+	if (error) {
+		return next({
+			type: failureType,
+			error: error.message,
+		})
+	}
+
+	if (data) {
+		return next({
+			type: succeededType,
+			payload: data,
+		})
+	}
 }
